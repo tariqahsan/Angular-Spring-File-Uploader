24805f9af2c4b148fe9792a2061346e603349858
Tariq Ahsan <tariqahsan@yahoo.com>
2021-11-06 11:16:39 -0400

    first commit

---
+++ file-upload-download-api/src/main/java/org/mma/training/java/springboot/resource/FileController.java
@@ -0,0 +1,75 @@
+package org.mma.training.java.springboot.resource;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.Files.copy;
+import static java.nio.file.Paths.get;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
+
+/**
+ * @author Tariq Ahsan
+ * @version 1.0
+ * @since 10/11/2021
+ */
+
+@RestController
+@RequestMapping("/api/v1/file")
+public class FileController {
+
+	private static Logger logger = LoggerFactory.getLogger(FileController.class);
+	
+    // define a location
+    public static final String DIRECTORY = System.getProperty("user.home") + "/Downloads/Uploads/";
+
+    // Define a method to upload files
+    @PostMapping("/upload")
+    public ResponseEntity<List<String>> uploadFiles(@RequestParam("files")List<MultipartFile> multipartFiles) throws IOException {
+        List<String> filenames = new ArrayList<>();
+        for(MultipartFile file : multipartFiles) {
+            String filename = StringUtils.cleanPath(file.getOriginalFilename());
+            logger.info(filename);
+            Path fileStorage = get(DIRECTORY, filename).toAbsolutePath().normalize();
+            logger.info(fileStorage.toString());
+            copy(file.getInputStream(), fileStorage, REPLACE_EXISTING);
+            filenames.add(filename);
+        }
+        return ResponseEntity.ok().body(filenames);
+    }
+
+    // Define a method to download files
+    @GetMapping("download/{filename}")
+    public ResponseEntity<Resource> downloadFiles(@PathVariable("filename") String filename) throws IOException {
+        Path filePath = get(DIRECTORY).toAbsolutePath().normalize().resolve(filename);
+        if(!Files.exists(filePath)) {
+            throw new FileNotFoundException(filename + " was not found on the server");
+        }
+        Resource resource = new UrlResource(filePath.toUri());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("File-Name", filename);
+        httpHeaders.add(CONTENT_DISPOSITION, "attachment;File-Name=" + resource.getFilename());
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(Files.probeContentType(filePath)))
+                .headers(httpHeaders).body(resource);
+    }
+
+
+}
---
+++ file-upload-download-api/src/test/java/org/mma/training/java/springboot/FileUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mma.training.java.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
